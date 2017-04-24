fe261f176af312fe635343a800709b26f925a8b0
Maxim Kanashin <maxkanashin@mail.ru>
2017-04-25 00:29:41 +0300

    first commit

---
+++ Parser/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(200, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ShortcutsEnabled = false;
+            this.richTextBox1.Size = new System.Drawing.Size(209, 311);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 127);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сайты";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(104, 25);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Meteoinfo";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 25);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Yandex";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(101, 25);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "GisMeteo";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(419, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(344, 310);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Сделал: Канашин Максим БИс-31";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 331);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Погода";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Parser/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace Parser
+{
+    public partial class Form1 : Form
+    {
+        public string[] Mass = new string[10];
+        public string[] _Day = new string[10];
+        public WebClient gm = new WebClient();
+
+        public String Gismeteo()
+        {
+            String Response = gm.DownloadString("https://www.gismeteo.ru/weather-yoshkar-ola-11975/weekly/");
+            string[] MaxT = new string[8];
+            string[] MinT = new string[8];
+            string[] Day = new string[8];
+            string[] DayD = new string[8];
+            string ret = "";
+            int i = 0;
+            // <em class="(date_of_week\s+[A-Za-z]+)">([0-9]+)</em>
+            foreach (Match match in Regex.Matches(Response, @"<em class=""(date_of_week\s+[A-Za-z]+)"">([0-9]+)</em>"))
+            {
+                DayD[i] = match.Groups[2].Value;
+                _Day[i] = DayD[i];
+                i++;
+            }
+            i = 0;
+            foreach (Match match in Regex.Matches(Response, @"class=""weather_item js_temp_graph""\s+data-max=""([-+]?[0-9]+)"" data-min=""([-+]?[0-9]+)"""))
+            {
+                MaxT[i] =  match.Groups[1].Value;
+                Mass[i] = MaxT[i];
+                MinT[i] =  match.Groups[2].Value;
+                i++;
+            }
+            i = 0;
+            foreach (Match match in Regex.Matches(Response, @"<span class=""day_of_week"">([А-Яа-я]+)</span>"))
+            {
+                 Day[i] = match.Groups[1].Value;
+                 i++;
+            }
+            for (int c = 0; c < 7; c++)
+            {
+                ret = ret + "---------- " + Day[c] + ", " + DayD[c] + " ----------\nmax T: " + MaxT[c] + " °С | min T: " + MinT[c] + " °С\n";
+            }
+            return ret;
+        }
+
+        public String Yandex()
+        {
+            String Response = gm.DownloadString("https://yandex.ru/pogoda/yoshkar-ola");
+            String Response1 = gm.DownloadString("https://yandex.ru/pogoda/yoshkar-ola/details");
+            string[] MaxT = new string[10];
+            string[] Day = new string[10];
+            string[] DayD = new string[10];
+            string ret = "";
+            int i = 0;
+            // <div class="forecast-brief__item-temp-day" title="Максимальная температура днём">+2 днём</div>
+            foreach (Match match in Regex.Matches(Response, @"<div class=""forecast-brief__item-temp-day"" title=""Максимальная температура днём"">([−+]?[0-9]+) днём</div>"))
+            {
+                MaxT[i] = match.Groups[1].Value;
+                Mass[i] = MaxT[i];
+                i++;
+            }
+            foreach (Match match in Regex.Matches(Response, @"<div class=""forecast-brief__item-temp-day"" title=""Максимальная температура днём"">([−+]?[0-9]+)</div>"))
+            {
+                MaxT[i] = match.Groups[1].Value;
+                Mass[i] = MaxT[i];
+                i++;
+            }
+            i = 0;
+            //<span class="forecast-brief__item-day-name">сегодня</span>
+            //<span class="forecast-brief__item-day">([0-9]+)</span>
+            //<strong class="forecast-detailed__day-number">([0-9]+)
+            foreach (Match match in Regex.Matches(Response1, @"<strong class=""forecast-detailed__day-number"">([0-9]+)"))
+            {
+                DayD[i] = match.Groups[1].Value;
+                _Day[i] = DayD[i];
+                i++;
+            }
+            i = 0;
+            foreach (Match match in Regex.Matches(Response, @"<span class=""forecast-brief__item-day-name"">([А-Яа-я]+)</span>"))
+            {
+                Day[i] = match.Groups[1].Value;
+                i++;
+            }
+            for (int c = 0; c < 7; c++)
+            {
+                ret = ret + "------- " + Day[c] + ", " + DayD[c] + " -------\nT: " + MaxT[c] + " °С\n";
+            }
+            return ret;
+        }
+
+        public String Meteoinfo()
+        {
+            String Response = gm.DownloadString("http://www.meteoinfo.ru/forecasts5000/russia/republic-mary-el/joskar-ola");
+            string[] MaxT = new string[7];
+            string[] MinT = new string[7];
+            string[] Day = new string[7];
+            string[] DayD = new string[7];
+            string ret = "";
+            int i = 0;
+            // <div class="forecast-brief__item-temp-day" title="Максимальная температура днём">+2 днём</div>
+            foreach (Match match in Regex.Matches(Response, @"<td align=center>([А-Яа-я]+)<BR><nobr>([0-9]+)\s([А-Яа-я]+)</nobr></td>"))
+            {
+                Day[i] = match.Groups[1].Value;
+                DayD[i] = match.Groups[2].Value;
+                _Day[i] = DayD[i];
+                i++;
+            }
+            foreach (Match match in Regex.Matches(Response, @"<td class=pogodadate>([А-Яа-я]+)<BR><nobr>([0-9]+)\s([А-Яа-я]+)</nobr></td>"))
+            {
+                Day[i] = match.Groups[1].Value;
+                DayD[i] = match.Groups[2].Value;
+                _Day[i] = DayD[i];
+                i++;
+            }
+            i = 0;
+            //<td class=pogodacell>([-+]?[0-9]) &nbsp;&frasl;&nbsp; ([-+]?[0-9])</td>
+            foreach (Match match in Regex.Matches(Response, @"<td class=pogodacell>([-+]?[0-9]{1,2}) &nbsp;&frasl;&nbsp; ([-+]?[0-9]{1,2})</td>"))
+            {
+                MinT[i] = match.Groups[1].Value;
+                MaxT[i] = match.Groups[2].Value;
+                Mass[i] = MaxT[i];
+                i++;
+            }
+            for (int c = 0; c < 7; c++)
+            {
+                ret = ret + "----- " + Day[c] + ", " + DayD[c] + " -----\nmax T: " + MaxT[c] + " °С | min T: " + MinT[c] + " °С\n";
+            }
+            return ret;
+        }
+
+        private void Draw()
+        {
+            //340 300
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            Pen penR = new Pen(Color.Red, 2);
+            Pen point = new Pen(Color.Red);
+            Pen os = new Pen(Color.Black);
+            int[] pointsX = new int[7];
+            int[] pointsY = new int[7];
+            Font drawFont = new Font("Calibri", 10);
+            
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            int x = 5, p = 32;
+            os.Width = 2;
+            //graph.DrawLine(pen, 15, 20, 375, 20);
+            graph.DrawLine(os, 30, 145, 325, 145);
+            graph.DrawLine(os, 30, 15, 30, 295);
+            for (int i = 0; i < 17; i++)
+            {
+                graph.DrawLine(os, 25, 20 + x, 35, 20 + x);
+                if(p>0)
+                    graph.DrawString("+" + p.ToString(), drawFont, drawBrush, 0, 10 + x);
+                else
+                    graph.DrawString(p.ToString(), drawFont, drawBrush, 0, 10 + x);
+                p -= 4;
+                x += 15;
+            }
+            x = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                graph.DrawLine(os, 30 + x, 140, 30 + x, 150);
+                graph.DrawString(_Day[i], drawFont, drawBrush, 30 + x, 142);
+                x += 40;
+            }
+            x = 0;
+            for (int m = 0; m < 7; m++)
+            {
+                Mass[m] = Mass[m].Replace("−", "-");
+                graph.FillRectangle(Brushes.Red, 27 + x, 142 - Convert.ToInt32(Mass[m])*4, 6, 6);
+                pointsX[m] = 27 + x;
+                pointsY[m] = 142 - Convert.ToInt32(Mass[m]) * 4;
+                x += 40;
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                graph.DrawLine(penR, pointsX[i] + 3, pointsY[i] + 3, pointsX[i+1] + 3, pointsY[i+1] + 3);
+                x += 40;
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            this.MaximizeBox = false;
+            gm.Proxy = new WebProxy("83.239.58.162:8080");
+            gm.Encoding = Encoding.UTF8;            
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();            
+            richTextBox1.Text = Gismeteo();
+            Draw();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.Text = Yandex();  
+            Draw();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.Text = Meteoinfo();
+            Draw();
+        }
+    }
+}
